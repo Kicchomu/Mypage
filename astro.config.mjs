374d293fe6a4d10db6ade0b9b3b6698326cbded8
kikkawa-chomu <K2531036@gl.cc.uec.ac.jp>
2026-05-24 21:56:44 +0900

    大規模改修

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // GitHub Pages deploy config. Adjust base if you are deploying to a sub-path.
+  site: 'https://kikkawa-chomu.github.io', 
+  base: '/Mypage',
+});
